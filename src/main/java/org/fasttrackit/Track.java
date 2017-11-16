d5abdd2b9494b90d78bf8d92d1d0e62e8348d45e
IoanaAndreeaAncuta <ancuta.ioana@ymail.com>
2017-11-16 21:26:34 +0200

    curs 4

---
+++ src/main/java/org/fasttrackit/Track.java
@@ -0,0 +1,7 @@
+package org.fasttrackit;
+
+public class Track {
+
+    double length;
+
+}
---
+++ src/main/java/org/fasttrackit/Vehicle.java
@@ -0,0 +1,35 @@
+package org.fasttrackit;
+
+public class Vehicle {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name.trim();
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+
+
+    private String name;
+    private String color;
+
+    public double accelerate(double speed) {
+
+        System.out.println(speed);
+
+        return 10.1;
+
+    }
+
+
+}
+
