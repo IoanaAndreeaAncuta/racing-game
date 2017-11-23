159dfdd5aaa22390c3c71b18b2334c583a7f548e
IoanaAndreeaAncuta <ancuta.ioana@ymail.com>
2017-11-23 19:31:16 +0200

    curs 4

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -5,29 +5,29 @@ public class App {
     public static void main(String[] args) {
         Race race = new Race();
         Track track = new Track();
-        track.length = 100.5;
+        track.setLength(100);
 
-        race.track = new Track();
+
         Engine engine = new Engine();
-        engine.manufacturer = "Volvo";
+        engine.setManufacturer("Volvo");
 
         Car car1 = new Car(engine);
         car1.setName("Volvo");
         car1.setColor("Black");
         car1.setFuelLevel(100.0);
         car1.setMileage(12.5);
-        car1.doorCount = 4;
+        car1.setDoorCount(4);
 
 
         Engine engine2 = new Engine();
-        engine2.manufacturer = "Honda";
+        engine2.setManufacturer("Honda");
 
         Car car2 = new Car(engine2);
         car2.setName("Honda");
         car2.setColor("Red");
         car2.setFuelLevel(100.0);
         car2.setMileage(13.5);
-        car2.doorCount = 2;
+        car2.setDoorCount(2);
 
 
 
